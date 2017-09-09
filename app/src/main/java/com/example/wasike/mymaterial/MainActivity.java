b7a15950fdd35f607cda0e4c75e93e0e7cf43fa1
Christine Wasike <christinewasike22@gmail.com>
2017-09-09 10:27:36 +0300

    Adding code on the tabs rendering the three fragments

--- app/src/main/java/com/example/wasike/mymaterial/MainActivity.java
+++ app/src/main/java/com/example/wasike/mymaterial/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.wasike.mymaterial;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.test.ViewAsserts;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,17 @@ public class MainActivity extends AppCompatActivity {
         //Adding Toolbar to Main Screen
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Setting ViewPager for each Tab
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        set
+
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("Tab 1"));
+        tabs.addTab(tabs.newTab().setText("Tab 2"));
+        tabs.addTab(tabs.newTab().setText("Tab 3"));
+
+
+
     }
 }
